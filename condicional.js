ae77dfbf7fe5be789a6d4d754b410d6c5ba64586
Samyra Lopes <samylopes28@gmail.com>
2019-02-12 13:54:41 -0200

    Salvar a introdução de js

---
+++ condicional.js
@@ -0,0 +1,10 @@
+/*let valores = [20, 3, 5, 8, 10];
+
+for(let valor of valores) {
+    if(valor % 2 === 0) {
+        console.log(`O número ${valor} é par.`)
+    }
+    else {
+        console.log(`O número ${valor} é impar.`)
+    }
+}*/
---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log("Hello world!");
+
+// let nota = prompt("Qual a nota do aluno?");
+
+// console.log(nota);
+
+//alert("A npta do aluno é: " + nota);
+//alert(`A nota do aluno é ${nota}.`);
+
+// if (Number(nota >=6)) {
+    // alert('Parabens, o aluno foi aprovado!');
+// }
+
+//else if = se não se
+// else if (nota >= 5.5) {
+    // alert('Parabéns, você foi aprovado, agradeça o sistema.')
+// }
+
+// else {
+    // alert('Parece que sua nota foi insuficiente');
+//  }
+
+// let valores = [20, 3, 5, 8, 10];
+
+// for(let valor of valores) {
+    // if(valor % 2 === 0) {
+        // console.log(`O número ${valor} é par.`)
+    // }
+    // else {
+        // console.log(`O número ${valor} é impar.`)
+    // }
+// }
+
+let tamanho = Number(prompt('Qual tamanho da escada você deseja?'));
+
+for (let i = 0; i < tamanho; i++) {
+   console.log("#");
+}
